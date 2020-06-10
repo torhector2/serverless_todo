aff11623712e6826618c4515b04fc1e701d3a5ae
Hector Rodriguez Fornies <torhector2@gmail.com>
2020-06-10 21:11:02 +0200

    core - add a first draft to update todos

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -7,15 +7,37 @@ import {
 } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+const XAWS = AWSXRay.captureAWS(AWS)
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  const docClient = new XAWS.DynamoDB.DocumentClient()
 
   todoId //avoid sls compiling errors
   updatedTodo //avoid sls compiling errors
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  var params = {
+    TableName: todosTable,
+    Key: {
+      partitionKey: todoId,
+      sortKey: '2020-07-11' //change this to be dynamic
+    },
+    UpdateExpression: 'set #n = :newName',
+    ExpressionAttributeValues: { ':newName': updatedTodo.name },
+    ExpressionAttributeNames: { '#n': 'name' }
+  }
+  await docClient.update(params).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      newItem: updatedTodo
+    })
+  }
 }
